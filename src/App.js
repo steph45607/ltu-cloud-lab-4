1407d05df96b57965a6e9b8d91a808ba03f3b1bd
choiruzain-latrobe <127911801+choiruzain-latrobe@users.noreply.github.com>
2024-02-21 22:35:13 +1100

    Add files via upload
    main-initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import List from './components/List';
+import './App.css';
+
+function App() {
+    return (
+        <div className='page'>     
+            <List heading='My Tasks'/>
+        </div>
+    );
+}
+
+export default App;
