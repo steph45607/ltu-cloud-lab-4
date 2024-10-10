b15456c1a5187c08da8cd6a241388466579505f7
steph45607 <staniswinata.stephanie@gmail.com>
2024-10-10 14:05:54 +0700

    added Task component and replace list to Task component

--- src/components/List.js
+++ src/components/List.js
@@ -1,8 +1,17 @@
+function Task(props){
+	return (
+		<li>{props.description}</li>
+	)
+}
+
 function List(props) {
 	return (
 		<div>
 			<h1>{ props.heading }</h1>
 			<ul>
+				<Task description="Anya List"/>
+				<Task description="Steph List"/>
+				<Task description="Stanis List"/>
 				<li>Learn React</li>
 				<li>Learn JSX</li>
 				<li>Build a React App</li>
