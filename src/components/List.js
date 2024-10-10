220ac10ea98a7226acf63648254e6b399ae24b50
steph45607 <staniswinata.stephanie@gmail.com>
2024-10-10 14:19:54 +0700

    completed list

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,6 @@
 function Task(props){
 	return (
-		<li>{props.description}<input type="checkbox" checked="false"/></li>
+		<li>{props.description}<input type="checkbox" checked={props.completed}/></li>
 	)
 }
 
@@ -9,9 +9,9 @@ function List(props) {
 		<div>
 			<h1>{ props.heading }</h1>
 			<ul>
-				<Task description={props.tasks[0].description}/>
-				<Task description={props.tasks[1].description}/>
-				<Task description={props.tasks[2].description}/>
+				<Task description={props.tasks[0].description} completed={props.tasks[0].completed}/>
+				<Task description={props.tasks[1].description} completed={props.tasks[1].completed}/>
+				<Task description={props.tasks[2].description} completed={props.tasks[2].completed}/>
 			</ul>
 		</div>
 	);
